c84136eafeebe3c4bdccf7593d6a4b1a149c5bc5
Gleisson de Assis <gleisson.assis@gmail.com>
2021-05-12 17:15:27 -0300

    Fixing minor bug

--- app.js
+++ app.js
@@ -18,31 +18,38 @@ app.post('/', async (req, res) => {
             'pinata_api_key': pinataAPIKey,
             'pinata_secret_api_key': pinataSecret,
         };
+
         const data = {
             pinataMetadata: {
                 name: 'Hashly Gwei Item',
-                keyvalues: req.body
+                keyvalues: {
+                    data: JSON.stringify(req.body),
+                },
             },
             pinataContent: req.body,
         }
-    
+
         try {
             console.log(`Pinning a new file`);
+            
             const pinata = await axios.post(pinataEndPoint, data, {
                 headers
             });
+
             console.log(`Content pinned successfully`);
+            console.log(pinata.data);
+            
             res.status(200);
             res.send({
                 ipfsHash: pinata.data.IpfsHash,
             });
         } catch (e) {
-            res.status(409);
-            res.send({ error: e.toString(), isError: true })
+            console.log(e);
+            res.send({ error: e.toString(), isError: true }).status(409);
         }
     } catch (e) {
-        res.status(500);
-        res.send({ error: e.toString() });
+        console.log(e);
+        res.send({ error: e.toString() }).status(500);
     }
 });
 
