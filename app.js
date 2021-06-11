7e61001cf7894711de8ef810b16c8f571544840f
Gleisson de Assis <gleisson.assis@gmail.com>
2021-06-11 08:31:07 -0300

    Adding collection name

--- app.js
+++ app.js
@@ -10,49 +10,48 @@ app.use(express.json());
 
 const pinataSecret = process.env.PINATA_SECRET;
 const pinataAPIKey = process.env.PINATA_API_KEY;
-const pinataEndPoint = process.env.PINATA_ENDPOINT;
-
 const port = process.env.PORT || 3000;
 
 app.post('/', async (req, res) => {
-    try {
-      const headers = {
-        'pinata_api_key': pinataAPIKey,
-        'pinata_secret_api_key': pinataSecret,
-      };
+  const url = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
+  try {
+    const headers = {
+      'pinata_api_key': pinataAPIKey,
+      'pinata_secret_api_key': pinataSecret,
+    };
 
-        const data = {
-            pinataMetadata: {
-                name: 'Hashly Gwei Item',
-                keyvalues: {
-                    data: JSON.stringify(req.body),
-                },
-            },
-            pinataContent: req.body,
-        }
-
-        try {
-            console.log(`Pinning a new file`);
-            
-            const pinata = await axios.post(pinataEndPoint, data, {
-                headers
-            });
-
-            console.log(`Content pinned successfully`);
-            console.log(pinata.data);
-            
-            res.status(200);
-            res.send({
-                ipfsHash: pinata.data.IpfsHash,
-            });
-        } catch (e) {
-            console.log(e);
-            res.send({ error: e.toString(), isError: true }).status(409);
-        }
-    } catch (e) {
-        console.log(e);
-        res.send({ error: e.toString() }).status(500);
+    const data = {
+      pinataMetadata: {
+        name: req.body.collection && req.body.collection.name || 'Hashly Gwei Item',
+        keyvalues: {
+            data: JSON.stringify(req.body),
+        },
+      },
+      pinataContent: req.body,
     }
+
+    try {
+      console.log(`Pinning a new file`);
+      
+      const pinata = await axios.post(url, data, {
+          headers
+      });
+
+      console.log(`Content pinned successfully`);
+      console.log(pinata.data);
+      
+      res.status(200);
+      res.send({
+          ipfsHash: pinata.data.IpfsHash,
+      });
+    } catch (e) {
+      console.log(e);
+      res.send({ error: e.toString(), isError: true }).status(409);
+    }
+  } catch (e) {
+    console.log(e);
+    res.send({ error: e.toString() }).status(500);
+  }
 });
 
 app.get('/', async (req, res) => {
